1c83bc981634c66e906e4cfa052edd52bddd7009
Henning <henningholm90@gmail.com>
2024-01-20 09:28:11 +0100

    Commit

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { beforeUpdate } from 'svelte';
+  import reportLogo from './assets/AiReport.png'
+  import TodoList from './TodoList.svelte';
+  const employers =["Thomas Teknikker", "Runar Rørlegger", "Even Elektrikker"]
+  let showEmployers = false;
+  let selectedEmployer = "";
+</script>
+
+<main>
+    <div>
+        <img src={reportLogo} class="logo svelte" alt="Svelte Logo"/>
+        <h1>ASTA AI PRO PLUS</h1>
+    </div>
+    <div>
+      {#if showEmployers || !selectedEmployer}
+      {#each employers as employer}
+      <br>
+        <button style="background-color: gray; font-size:large" on:click={()=>selectedEmployer=employer}>{employer}</button>
+      <br>
+      {/each}
+
+      {/if}
+    </div>
+    <div>
+      <br>
+    <button on:click={()=>showEmployers=!showEmployers}>{showEmployers ? "Close" :"Select Employer"}</button>
+    </div>
+    <div>
+      {#if selectedEmployer && !showEmployers}
+      <TodoList name={selectedEmployer}/>
+      {/if}
+
+    </div>
+</main>
+
+<style>
+  .logo {
+    height: 20em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+
+  }
+img{
+  padding: 0 !important;
+}
+
+
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/TodoList.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    let tasks = new Array<string>();
+    let newTask = '';
+    export let name :string;
+
+    function addTask() {
+        if (newTask.trim() !== '') {
+            tasks = [...tasks, newTask];
+            newTask = '';
+        }
+    }
+
+    function deleteTask(task: string) {
+        tasks = tasks.filter(t => t !== task);
+    }
+</script>
+
+<style>
+    .task-list {
+        list-style: none;
+        padding: 0;
+    }
+
+    .task-item {
+        margin: 10px 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    button {
+        margin-left: 10px;
+    }
+</style>
+
+<div>
+    <h1>{name}s Controll List</h1>
+    <input style="font-size: large;" type="text" bind:value={newTask} placeholder="Legg til ny oppgave" />
+    <button on:click={addTask}>Legg til</button>
+
+    <ul class="task-list">
+        {#each tasks as task}
+            <li class="task-item">
+                {task}
+                <button on:click={() => deleteTask(task)}>Slett</button>
+            </li>
+        {/each}
+    </ul>
+</div>
